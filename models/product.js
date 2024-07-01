55b681b69db263499999de3ba9aa486a64be6586
ChiragSingla12 <Chiragsingla1254@gmail.com>
2024-07-01 21:26:50 +0530

    mongodb connection start

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = imageUrl;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  save() {}
+}
+
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
---
+++ util/database.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+const { get } = require("../routes/admin");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://chiragsingla1245:1maQ46vmG3Vvx4Up@cluster0.ttzdufu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected!..");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No Database Found";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
