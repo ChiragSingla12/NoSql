225d3a29cefcb670216f238b322bf5b235302b76
ChiragSingla12 <Chiragsingla1254@gmail.com>
2024-07-04 15:47:34 +0530

    mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title,
-    price,
-    imageUrl,
-    description,
-    null,
-    req.user._id
-  );
+  const product = new Product({
+    title: title,
+    imageUrl: imageUrl,
+    price: price,
+    description: description,
+  });
   product
     .save()
     .then((result) => {
@@ -61,15 +59,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedimageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedimageUrl,
-    updatedDesc,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedimageUrl;
+      product.updatedDesc = updatedDesc;
+      product.save();
+    })
     .then((result) => {
       console.log("UPDATED PRODUCT!");
       res.redirect("/admin/products");
@@ -78,7 +75,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -91,7 +88,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then(() => {
       console.log("DESTROYED the PRODUCT");
       res.redirect("/admin/products");
--- util/database.js
+++ util/database.js
@@ -1,29 +1,29 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
 
-let _db;
+// let _db;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect(
-    "mongodb+srv://chiragsingla1245:1maQ46vmG3Vvx4Up@cluster0.ttzdufu.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
-  )
-    .then((client) => {
-      console.log("Connected!..");
-      _db = client.db();
-      callback();
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
-};
+// const mongoConnect = (callback) => {
+//   MongoClient.connect(
+//     "mongodb+srv://chiragsingla1245:1maQ46vmG3Vvx4Up@cluster0.ttzdufu.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+//   )
+//     .then((client) => {
+//       console.log("Connected!..");
+//       _db = client.db();
+//       callback();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       throw err;
+//     });
+// };
 
-const getDb = () => {
-  if (_db) {
-    return _db;
-  }
-  throw "No Database Found";
-};
+// const getDb = () => {
+//   if (_db) {
+//     return _db;
+//   }
+//   throw "No Database Found";
+// };
 
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb;
